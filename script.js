83c6af138c47fa2528114e62bc379e8422c834b7
Samriddhi Singh <ssamriddhi1308@gmail.com>
2025-02-03 00:15:36 +0530

    'fixed'

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
-let a = document.querySelector("h1");
-a.innerHTML = "Changed html";
-a.style.backgroundColor = "red";
-a.addEventListener("click", function () {
-    a.innerHTML = "Badal gaya hu mai teri ex ki tarah"
-})
-let flag = 0;
-x = document.querySelector("#bulb")
-x.addEventListener("click", function () {
-    if (flag == 0) {
-        x.style.backgroundColor = "yellow"
-        flag = 1
+let score = 0;
+let timer = 60;
+let hitrn = 0;
+
+function makeBubble() {
+    clutter = "";
+    for (let i = 0; i < 168; i++) {
+        rd = Math.floor(Math.random() * 10);
+        clutter += `<div class="bubble">${rd}</div>`
     }
-    else {
-        x.style.backgroundColor = "white"
-        flag = 0
+    document.querySelector("#pannelBottom").innerHTML = clutter;
+}
+
+function timeRun() {
+    let timeVal = setInterval(function () {
+        if (timer > 0) {
+            timer--;
+            document.querySelector("#time").textContent = timer;
+        }
+        else {
+            clearInterval(timeVal);
+            document.querySelector("#pannelBottom").innerHTML = `<h1>Game Over </h1>
+                                                                <h1>Your Score: ${score}</h1>`
+        }
+    }, 1000)
+}
+
+
+function getNewHit() {
+    hitrn = Math.floor(Math.random() * 10);
+    document.querySelector("#hitVal").textContent = hitrn;
+}
+
+function increaseScore() {
+    score += 10;
+    document.querySelector("#scoreVal").textContent = score;
+}
+
+
+document.querySelector("#pannelBottom").addEventListener("click", function (dets) {
+    let clicked = Number(dets.target.textContent);
+    if (clicked === hitrn) {
+        increaseScore();
+        getNewHit();
+        makeBubble();
     }
 })
+makeBubble();
+timeRun();
+getNewHit();
